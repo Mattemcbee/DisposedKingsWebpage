d47c8ff122038552dc5aaae89d134dc5956a7e3c
Matt McBee <mattemcbee@gmail.com>
2022-11-15 21:14:50 -0500

    upload 4

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+
+//change style
+$(function () {
+    $('#addEmail').on('click', function () {
+        event.preventDefault();
+        console.log("thanks!");
+    });
+})
+
+$(document).ready(function() {
+    
+  //brandon
+  $('#brandon').mouseover(function() {
+  $('#brandon').hide();
+  $('#brandon2').show();
+  });
+  $('#brandon2').mouseout(function() {
+  $('#brandon2').hide();
+  $('#brandon').show();
+  });
+  
+  //zack
+  $('#zack').mouseover(function() {
+  $('#zack').hide();
+  $('#zack2').show();
+  });
+  $('#zack2').mouseout(function() {
+  $('#zack2').hide();
+  $('#zack').show();
+  });
+  
+  //zack
+  $('#troy').mouseover(function() {
+  $('#troy').hide();
+  $('#troy2').show();
+  });
+  $('#troy2').mouseout(function() {
+  $('#troy2').hide();
+  $('#troy').show();
+  });
+  
+   //jacob
+  $('#jacob').mouseover(function() {
+  $('#jacob').hide();
+  $('#jacob2').show();
+  });
+  $('#jacob2').mouseout(function() {
+  $('#jacob2').hide();
+  $('#jacob').show();
+  });
+
+});
